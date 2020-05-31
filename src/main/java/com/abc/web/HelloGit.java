63a4569d440c5dca6cc74c1bda54a92c1b5db6db
DuanYuqQing <yduan1993@aliyun.com>
2020-05-31 21:19:19 +0800

    create project.

---
+++ src/main/java/com/abc/web/HelloGit.java
@@ -0,0 +1,7 @@
+package com.abc.web;
+
+public class HelloGit {
+    public void print() {
+        System.out.println("Hello Git!");
+    }
+}
---
+++ src/test/java/com/abc/HelloGitTest.java
@@ -0,0 +1,10 @@
+package com.abc;
+
+import com.abc.web.HelloGit;
+
+public class HelloGitTest {
+    public static void main(String[] args) {
+        HelloGit helloGit = new HelloGit();
+        helloGit.print();
+    }
+}
