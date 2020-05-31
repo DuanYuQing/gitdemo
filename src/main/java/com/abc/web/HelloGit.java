436a42de63d42c838b2a9824c9653492a80e792b
StephenTwan <yduan1993@outlook.com>
2020-05-31 21:27:20 +0800

    小袁修改了部分源码。

--- src/main/java/com/abc/web/HelloGit.java
+++ src/main/java/com/abc/web/HelloGit.java
@@ -2,6 +2,7 @@ package com.abc.web;
 
 public class HelloGit {
     public void print() {
-        System.out.println("Hello Git!");
+        // 小袁修改了部分源码
+        System.out.println("Hello Git!小袁修改了部分源码");
     }
 }
