a852cf73f9c43e50949444deba2d7ea20661ec5d
StephenTwan <yduan1993@outlook.com>
2020-05-31 22:23:46 +0800

    小袁接受了一个新功能的任务，创建了一个分支并在分支上开发

--- src/main/java/com/abc/web/HelloGit.java
+++ src/main/java/com/abc/web/HelloGit.java
@@ -3,6 +3,7 @@ package com.abc.web;
 public class HelloGit {
     public void print() {
         // 小袁修改了部分源码
+        // 小袁接受了一个新功能的任务，创建了一个分支并在分支上开发
         System.out.println("Hello Git!小袁修改了部分源码");
     }
 }
